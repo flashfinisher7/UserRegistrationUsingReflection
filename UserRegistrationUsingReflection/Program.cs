fa33b93da766767deacf2f9e34c083d8f37577d3
flashfinisher7 <68723204+flashfinisher7@users.noreply.github.com>
2021-08-28 15:55:42 +0530

    Add project files.

---
+++ UserRegistrationUsingReflection/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace UserRegistrationUsingReflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t\t\t\tUser Registration using Reflection");
+            ValidateUserDetails validate = new ValidateUserDetails();
+            validate.ValidateUser();
+        }
+    }
+}
